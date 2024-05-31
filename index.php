98df91cacac20b0b5849c6ddfcdc494254bd0b6d
Manopdemaan <112510226+Manopdemaan@users.noreply.github.com>
2024-05-31 13:33:36 +0200

    m

--- index.php
+++ index.php
@@ -12,27 +12,27 @@
                 <p><a href="new_post.php">Nieuwe post</a></p>
             </div>
             <?php
-require_once 'connection.php';
+               require_once 'connection.php';
 
-try {
+            try {
 
-    $query = "SELECT * FROM posts";
-    $stmt = $conn->query($query);
+                $query = "SELECT * FROM posts";
+                $stmt = $conn->query($query);
 
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        echo "<div class='post'>";
-        echo "<div class='header'>";
-        echo "<h2>" . htmlspecialchars($row['titel']) . "</h2>";
-        echo "<img src='" . htmlspecialchars($row['img_url']) . "' alt='" . htmlspecialchars($row['titel']) . "'>";
-        echo "</div>";
-        echo "<span class='details'>Geschreven op: " . htmlspecialchars($row['datum']) . " door <b>" . htmlspecialchars($row['auteur']) . "</b></span>";
-        echo "<p>" . nl2br(htmlspecialchars($row['inhoud'])) . "</p>";
-        echo "</div>";
-    }
-} catch (PDOException $e) {
-    echo "Error: " . $e->getMessage();
-}
-?>
+                while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                    echo "<div class='post'>";
+                    echo "<div class='header'>";
+                    echo "<h2>" . htmlspecialchars($row['titel']) . "</h2>";
+                    echo "<img src='" . htmlspecialchars($row['img_url']) . "' alt='" . htmlspecialchars($row['titel']) . "'>";
+                    echo "</div>";
+                    echo "<span class='details'>Geschreven op: " . htmlspecialchars($row['datum']) . " door <b>" . htmlspecialchars($row['auteur']) . "</b></span>";
+                    echo "<p>" . nl2br(htmlspecialchars($row['inhoud'])) . "</p>";
+                    echo "</div>";
+                }
+           } catch (PDOException $e) {
+                echo "Error: " . $e->getMessage();
+            }
+          ?>
         </div>
     </body>
 </html>
